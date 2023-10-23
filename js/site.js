3450ab1a2ff5629b182d2a9e500988999dbd08a2
Thomas Bell <thomasjbell065@gmail.com>
2023-10-23 17:36:56 -0400

    made corrections where i could, still not working

--- js/site.js
+++ js/site.js
@@ -1,17 +1,88 @@
-function displayMessage() {
-    // get the text out of our <input> and assign it to a variable
-    let msg = document.getElementById('message').value;
+// get the user's input
+function getUserInfo() {
+    let loanAmount = document.getElementById('loanAmount').value;
+    let monthlyTerm = document.getElementById('monthlyTerm').value;
+    let interestRate = document.getElementById('interestRate').value;
 
-    // alert('Hello Guardian Cohort!')
-    Swal.fire(
-        {
-            icon: 'success',
-            backdrop: false,
-            title: '<strong>APP NAME</strong>',
-            text: msg, // <---- replace this text with that variable
-            showConfirmButton: false,
-            timer: 1500,
-            timerProgressBar: true,
-        }
-    );
+    loanAmount = parseInt(loanAmount);
+    monthlyTerm = parseInt(monthlyTerm);
+    interestRate = parseInt(interestRate);
+
+    let newHome = calculateLoan(loanAmount, monthlyTerm, interestRate);
+    displayStats(calculateLoan(newHome));
+}
+
+// do the calculations
+function calculateLoan(loan, term, interest) {
+    // Convert annual interest rate to monthly by 12 and 100 to convert from percentage to a whole decimal number
+    const monthlyInterestRate = interest / 1200;
+
+    // calculate the number of monthly payments
+    const numberOfPayments = term;
+
+    // calculate rediculous math I had to look up
+    let totalPrincipal = 0;
+    let totalInterest = 0;
+
+    let numerator = loan * monthlyInterestRate;
+    let denominator = 1 - Math.pow(1 + monthlyInterestRate, -term);
+
+    // calculate the monthly mortgage payment
+    const monthlyPayment = numerator / denominator;
+
+    for (let month = 1; month <= numberOfPayments; month++) {
+        // interest
+        const interestPayment = loan * monthlyInterestRate;
+        // principal
+        const principalPayment = monthlyPayment - interestPayment;
+        
+        totalPrincipal += principalPayment;
+        totalInterest += interestPayment;
+        loanAmount -= principalPayment;
+        balance -= monthlyBalance;
+    }
+    let totalCost = totalPrincipal + totalInterest;
+    // balance
+    const monthlyBalance = totalCost - monthlyPayment
+
+    return {
+        monthlyPayment: monthlyPayment,
+        totalPrincipal: totalPrincipal,
+        totalInterest: totalInterest,
+        totalCost: totalCost,
+        monthlyBalance: monthlyBalance
+    }
+}
+
+// display table on the page
+function displayLoan(stats) {
+    displayStats(params)
+
+    let data = document.getElementById('calculation-table');
+    data.innerHTML = '';
+
+    const dataTemplate = document.getElementById('calculate-data');
+    dataTemplate.content.cloneNode(true);
+
+    document.getElementById('payment').textContent = stats.monthlyPayment;
+    document.getElementById('principle').textContent = stats.totalPrincipal;
+    document.getElementById('interest').textContent = stats.totalInterest;
+    document.getElementById('balance').textContent = stats.monthlyBalance
+
+    let html = '';
+
+    let tbody = document.getElementById('calculation-table');
+    tbody.innerHTML = html;
+
+    data.appendChild(dataTemplate)
+}
+
+// display stats
+function displayStats(params) {
+    let stats = calculateLoan(params);
+
+    document.getElementById('totalPrincipal').textContent = stats.totalPrincipal;
+    document.getElementById('totalInterest').textContent = stats.totalInterest;
+    document.getElementById('totalCost').textContent = stats.totalCost;
+    document.getElementById('displayPage').textContent = stats.monthlyPayment
 }
